10edfae7bcbc43f7704fb117d607f223b41a6c6d
Freeman <yfreeman102804@gmail.com>
2025-05-05 22:04:44 -0700

    Added comments

--- MergeSort.py
+++ MergeSort.py
@@ -1,13 +1,22 @@
 import random
 import time
 
+#------------------------------------------------------------------------------------
+#---Generates a an array with size N filled with values between 0 to 100 inclusive---
+#------------------------------------------------------------------------------------
 def gen_lst(A,n):
     for i in range(n):
         A.append(random.randint(0,100))
 
+#-------------------------------------
+#---Return the kth smallest element---
+#-------------------------------------
 def return_kth(A,k):
     return A[k-1]
 
+#---------------------------------
+#---Mergeing step of Merge Sort---
+#---------------------------------
 def merge(A, low, mid, high):
     U = [0] * len(A)
     i,j,k = low, mid+1, low
@@ -31,6 +40,9 @@ def merge(A, low, mid, high):
     for p in range(low, high+1):
         A[p] = U[p]
 
+#------------------------------------------
+#---Divide and Conquer step of MergeSort---
+#------------------------------------------
 def mergeSort(A,low,high):
     if low < high:
         mid = (low + high) // 2
@@ -39,16 +51,20 @@ def mergeSort(A,low,high):
         merge(A, low, mid, high)
 
 if __name__ == '__main__':
+    # Initialization
     N = 2
     k = 1
     arr = []
 
+    # Randomize the list
     gen_lst(arr,N)
 
+    # Used for timing for the algorithm
     start = time.time()
     mergeSort(arr,0,N-1)
     return_kth(arr,k)
     timeElapsed = time.time() - start
 
+    # Outputs
     print(f"---Time elapsed = {timeElapsed} seconds")
     print(f"\n{k}th smallest element: {return_kth(arr,k)}\n")
