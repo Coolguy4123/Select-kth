65b36e7744fb33255bc856f1222e6fea8440b545
Freeman <yfreeman102804@gmail.com>
2025-05-05 22:12:12 -0700

    Added comments

--- QuickSort.py
+++ QuickSort.py
@@ -1,19 +1,31 @@
 import random
 import time
 
+#------------------------------------------------------------------------------------
+#---Generates a an array with size N filled with values between 0 to 100 inclusive---
+#------------------------------------------------------------------------------------
 def gen_lst(A,n):
     for i in range(n):
         A.append(random.randint(0,100))
 
+#-------------------------------------
+#---Return the kth smallest element---
+#-------------------------------------
 def return_kth(A,k):
     return A[k-1]
 
+#--------------------------------------------
+#---Divide and Conquer Step for Quick Sort---
+#--------------------------------------------
 def quickSort(A,p,q):
     if p < q:
         pivotPos = partition(A,p,q)
         quickSort(A,p,pivotPos-1)
         quickSort(A,pivotPos+1,q)
 
+#--------------------------------------------------------------
+#---Partioning step of QuickSort, based on the first element---
+#--------------------------------------------------------------
 def partition(A, low, high):
     v, j = A[low], low
     for i in range(low + 1, high+1):
@@ -26,16 +38,20 @@ def partition(A, low, high):
 
 
 if __name__ == '__main__':
+    # Initialization
     N = 2
     k = 1
     arr = []
 
+    # Randomize the list
     gen_lst(arr,N)
 
+    # Timing of the algrithm
     start = time.time()
     quickSort(arr,0,N-1)
     return_kth(arr,k)
     timeElapsed = time.time() - start
 
+    # Outputs
     print(f"---Time elapsed = {timeElapsed} seconds")
     print(f"\n{k}th smallest element: {return_kth(arr,k)}\n")
